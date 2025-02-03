12551328cb82604b3638c477876835c4611b3b73
Huron <charlie.walls97@gmail.com>
2025-02-03 13:49:13 -0500

    add more thing about of variable declaration

--- basics/types.ts
+++ basics/types.ts
@@ -4,5 +4,13 @@ let isIT: boolean=true;
 let skils:Object={};
 
 let fruts: string[]=["manzana","pera"];
-let cars:Object[]=[{},{name:"toyota"}]
+let cars:Object[]=[{},{name:"toyota"}];
 
+let cualquier:any ="hola";
+
+function saludar():void{
+    console.log("Hola")
+}
+
+// definicion de variables con or
+let response5: string | null;
