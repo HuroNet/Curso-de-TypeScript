6bc0d8b55b0692c408a9d1855a53d8e6251fe047
Huron <charlie.walls97@gmail.com>
2025-02-03 11:56:15 -0500

    primer commit

--- basics/types.ts
+++ basics/types.ts
@@ -1,1 +1,8 @@
-let edad: number = 19
+let edad: number = 19;
+let nombre:string="carlos";
+let isIT: boolean=true;
+let skils:Object={};
+
+let fruts: string[]=["manzana","pera"];
+let cars:Object[]=[{},{name:"toyota"}]
+
