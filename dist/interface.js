1c05d54534f3c90f319996aae6b481a66342b51b
Huron <charlie.walls97@gmail.com>
2025-02-07 11:42:21 -0500

    Finalizado la clase de interface

--- dist/interface.js
+++ dist/interface.js
@@ -2,7 +2,10 @@ var usuario = {
     name: 'Calo',
     code: '01',
     cell: 182,
+    description: "hola",
 };
 for (var value in usuario) {
+    console.log(usuario[value]);
     console.log(value);
 }
+usuario.code = 50;
